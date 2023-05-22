db31e894a3a5c7c4a2bbcebce4afcfb885199af1
Kulpot <kulpotwahu@gmail.com>
2023-05-23 03:39:14 +0800

    Add project files.

---
+++ AutoProperties/Contact.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoProperties
+{
+    class Contact
+    {
+        // Externally mutable properties
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        // Read-only properties
+        public int PhoneNumber { get; private set; }
+        public string PostalCode { get; private set; }
+
+        public Contact(string contactName, string contactAddress)
+        {
+            Name = contactName;
+            Address = contactAddress;
+        }
+    }
+}
---
+++ AutoProperties/MyStats.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoProperties  // Get set accessors of Properties
+{
+    class MyStats
+    {
+        private int _age;
+
+        public MyStats(int age)
+        {
+            _age = age;
+        }
+
+        /// <summary>
+        /// Typical set Method
+        /// </summary>
+        public void SetAge(int age)
+        {
+            _age = age;
+        }
+
+        public int Age
+        {
+            get { return  _age; }
+            set
+            {
+                // Make age a valid positive number
+                if(_age <= 0)
+                {
+                    _age = 1;
+                }
+                else
+                {
+                    _age = value;
+                }
+            }
+        }
+    }
+}
